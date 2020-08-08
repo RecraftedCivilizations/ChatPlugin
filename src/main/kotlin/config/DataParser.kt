6fb57a39259da4b62ec64f0797385392be1bbf09
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-08 16:42:55 +0200

    feat: readData from config file

--- src/main/kotlin/config/DataParser.kt
+++ src/main/kotlin/config/DataParser.kt
@@ -1,4 +1,24 @@
 package com.github.DarkVanityOfLight.ChattPlugin.config
 
-class DataParser {
+import com.github.DarkVanityOfLight.ChattPlugin.Main
+import org.bukkit.Bukkit
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.configuration.file.YamlConfiguration
+import java.io.File
+
+class DataParser(main: Main) {
+    var dataFile : File = File(main.dataFolder.toString() + "data.yml")
+
+    fun readData() : Map<String, String> {
+        val config : YamlConfiguration = YamlConfiguration.loadConfiguration(dataFile)
+        val playerChatData : MutableMap<String, String> = emptyMap<String, String>().toMutableMap()
+        val configSection : ConfigurationSection? = config.getConfigurationSection("PlayerChatData")
+
+        if (configSection != null)
+            for (key : String in configSection.getKeys(false)){
+                playerChatData[key] = configSection.getString("PlayerChatData $key").toString()
+            }
+
+        return playerChatData
+    }
 }
