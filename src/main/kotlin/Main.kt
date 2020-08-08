370339c6fddb723a2ca86fdbd5fbc0aafaf44d0b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-08 18:48:46 +0200

    feat: Create chat obj for every Chat defined in the config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package com.github.DarkVanityOfLight.ChattPlugin
 
+import com.github.DarkVanityOfLight.ChattPlugin.config.configParser
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandExecutor
 import org.bukkit.event.EventHandler
@@ -10,6 +11,8 @@ import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
 class Main : JavaPlugin(), Listener, CommandExecutor{
+    val parser : configParser = configParser(this)
+    var chats : MutableList<Chatt> = mutableListOf()
 
     override fun onEnable(){
         saveDefaultConfig()
@@ -18,6 +21,20 @@ class Main : JavaPlugin(), Listener, CommandExecutor{
             val isCreated : Boolean = f.createNewFile()
             if (!isCreated) Bukkit.getLogger().warning("Could not create file ${dataFolder.toString()}data.yml")
         }
+
+        // Create chat obj for every chat defined in the config file
+        for ((index, channel) in parser.chats.withIndex()) {
+            val properties = parser.chatProperties[channel]
+            if (properties != null) {
+                if ("ignoreWorld" in properties.keys){
+                    chats[index] = Chatt(
+                            properties[name] as String, properties["list_style"] as String,
+                            properties["ignoreWorld"] as Boolean, properties["format"] as String,
+                            properties["muteable"] as Boolean, properties["radius"] as Int)
+                }
+            }
+        }
+
         Bukkit.getPluginManager().registerEvents(this, this)
     }
 
