f4dde8c4693105b9fc52f55295534324b8588b31
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-08 14:02:25 +0200

    feat: Check for ignore and broadcast message

--- src/main/kotlin/Chatt.kt
+++ src/main/kotlin/Chatt.kt
@@ -1,28 +1,37 @@
 package com.github.DarkVanityOfLight.ChattPlugin
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import kotlin.properties.Delegates
 
 class Chatt {
-    lateinit var format: String;
+    lateinit var format: String
+    var ignoreWorld = true
 
-    constructor(name : String, list_style : String, ignore_world : Boolean, format : String, muteable : Boolean){
+    constructor(name : String, list_style : String, ignoreWorld : Boolean, format : String, muteable : Boolean){
         this.format = format
+        this.ignoreWorld = ignoreWorld
     }
     constructor(name: String, list_style: String, format: String, muteable: Boolean){}
 
-    fun send_message(message : String, sender : Player){
-        var players : List<Player> = get_players_in_range(sender)
+    fun sendMessage(message : String, sender : Player){
+        var players : List<Player> = getPlayersInRange(sender)
+        var assmbledMessage : String = assembleMessage(message, sender)
 
-        for (player in players){
-            player.sendMessage(message)
+        if (ignoreWorld) {
+            Bukkit.getServer().broadcastMessage(assmbledMessage)
+        } else {
+            for (player in players){
+                player.sendMessage(message)
+            }
         }
     }
 
-    fun assemble_message(message: String, player : Player){
+    private fun assembleMessage(message: String, player : Player) : String{
 
     }
 
-    fun get_players_in_range(sender: Player) : List<Player>{
+    private fun getPlayersInRange(sender: Player) : List<Player>{
 
     }
 }
