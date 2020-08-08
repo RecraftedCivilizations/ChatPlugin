796ea4e069df1640e2846a84652635916ed05cde
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-08 18:49:25 +0200

    feat: Parse chat properties

--- src/main/kotlin/config/configParser.kt
+++ src/main/kotlin/config/configParser.kt
@@ -4,6 +4,7 @@ import com.github.DarkVanityOfLight.ChattPlugin.Main
 
 class configParser(val main: Main) {
     lateinit var chats: List<String>
+    lateinit var chatProperties : Map<String, Map<String, Any>>
 
     init {
         read()
@@ -11,6 +12,16 @@ class configParser(val main: Main) {
 
     private fun read(){
         chats = main.config.getList("Channels") as List<String>
+
+        for (chat in chats){
+            val properties = emptyMap<String, Any>().toMutableMap()
+
+            for (key in main.config.getConfigurationSection(chat)!!.getKeys(false)){
+                properties[key] = main.config.getConfigurationSection(chat)!![key] as Any
+            }
+
+            chatProperties = mapOf(Pair(chat, properties))
+        }
 
     }
 
