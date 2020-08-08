4ef83e6197785deb518b505f636f05014f9ca506
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-08 16:06:02 +0200

    feat: Check if data.yml is already created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,19 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class Main : JavaPlugin(), Listener, CommandExecutor{
 
     override fun onEnable(){
+
+        val f : File = File(dataFolder.toString() + "data.yml")
+        if (!f.exists()){
+            val isCreated : Boolean = f.createNewFile()
+            if (!isCreated) Bukkit.getLogger().warning("Could not create file ${dataFolder.toString()}data.yml")
+        }
         Bukkit.getPluginManager().registerEvents(this, this)
     }
 
