104f00dfa9045d91201a1cf08f615b0643a943ac
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-09 10:22:07 +0200

    fix: don't acces config everytime from main, get it from main and then use it

--- src/main/kotlin/config/ConfigParser.kt
+++ src/main/kotlin/config/ConfigParser.kt
@@ -2,27 +2,37 @@ package com.github.DarkVanityOfLight.ChattPlugin.config
 
 import com.github.DarkVanityOfLight.ChattPlugin.Main
 import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
 
 class ConfigParser(private val main: Main) {
     lateinit var chats: List<String>
     lateinit var chatProperties : Map<String, Map<String, Any>>
     lateinit var defaultChannel : String
+    lateinit var config : FileConfiguration
+
+    init {
+        main.saveDefaultConfig()
+        main.reloadConfig()
+
+    }
 
     fun read(){
-        chats = main.config.getList("Channels") as List<String>
+        config = main.getConfig()
+
+        chats = config.getList("Channels") as List<String>
 
         for (chat in chats){
             val properties = emptyMap<String, Any>().toMutableMap()
 
-            for (key in main.config.getConfigurationSection(chat)!!.getKeys(false)){
-                properties[key] = main.config.getConfigurationSection(chat)!![key] as Any
+            for (key in config.getConfigurationSection(chat)!!.getKeys(false)){
+                properties[key] = config.getConfigurationSection(chat)!![key] as Any
             }
 
             chatProperties = mapOf(Pair(chat, properties))
         }
 
-        if (main.config.getString("defaultChannel") != null) {
-            defaultChannel = main.config.getString("defaultChannel")!!
+        if (config.getString("defaultChannel") != null) {
+            defaultChannel = config.getString("defaultChannel")!!
 
         }else {
             Bukkit.getLogger().warning("You did not define a default channel")
