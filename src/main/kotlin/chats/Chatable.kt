06b285ca2d68d9d5a1006bf38c46cdea83dc65e9
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-11 20:35:34 +0200

    Create interface Chatable

---
+++ src/main/kotlin/chats/Chatable.kt
@@ -0,0 +1,11 @@
+package com.github.DarkVanityOfLight.ChattPlugin.chats
+
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+
+interface Chatable {
+    abstract var format : String
+
+    fun sendMessage()
+    fun assembleMessage(message: String, sender: Player)
+}
