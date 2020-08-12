541b0800c67d6ed024cbabdbfa85da1f412c07b3
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-12 10:51:47 +0200

    feat: add channelName with default to sendMessage args

--- src/main/kotlin/chats/Chatable.kt
+++ src/main/kotlin/chats/Chatable.kt
@@ -6,6 +6,6 @@ import org.bukkit.entity.Player
 interface Chatable {
     var format : String
 
-    fun sendMessage(message : String, sender : Player)
+    fun sendMessage(message : String, sender : Player, channelName: String? = null)
     fun assembleMessage(message: String, sender : Player, channelName: String? = null) : String
 }
