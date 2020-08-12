739e1a6ba59c42f4afb7972c41b66a7354f24bde
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-12 10:11:29 +0200

    fix: args for assembleMessage

--- src/main/kotlin/chats/Chatable.kt
+++ src/main/kotlin/chats/Chatable.kt
@@ -4,8 +4,8 @@ import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 interface Chatable {
-    abstract var format : String
+    var format : String
 
     fun sendMessage()
-    fun assembleMessage(message: String, sender: Player, channelName: String? = null) : String
+    fun assembleMessage(message: String, sender : Player, channelName: String?) : String
 }
