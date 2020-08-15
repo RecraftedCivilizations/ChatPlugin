0f39652e533f41acc9148d38a60e2dc7c463890a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-15 11:22:15 +0200

    fix: remove %discorduser because it may be null%

--- src/main/kotlin/chats/Chat.kt
+++ src/main/kotlin/chats/Chat.kt
@@ -21,26 +21,23 @@ abstract class Chat : Chatable {
             val factionTag = fPlayer.faction.tag
             val title = fPlayer.title
             val deaths = fPlayer.deaths.toString()
-            val discordUser = fPlayer.discordUser().name
 
             form.replace("%role_prefix%", rolePrefix)
             form.replace("%role%", role)
             form.replace("%faction_tag%", factionTag)
             form.replace("%title%", title)
             form.replace("%deaths%", deaths)
-            form.replace("%discord_user%", discordUser)
 
             message.replace("%role_prefix%", rolePrefix)
             message.replace("%role%", role)
             message.replace("%faction_tag%", factionTag)
             message.replace("%title%", title)
             message.replace("%deaths%", deaths)
-            message.replace("%discord_user%", discordUser)
 
 
         } else {
             Bukkit.getLogger().info("Factions plugin is not enabled on this server you cannot use:\n" +
-                    "%role_prefix%, %role%, %faction_tag%, %title%, %deaths%, %discord_user%")
+                    "%role_prefix%, %role%, %faction_tag%, %title%, %deaths%")
         }
 
         form = form.replace("%player_name%", sender.name)
