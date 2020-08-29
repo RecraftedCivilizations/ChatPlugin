1dcfc5f0a05468e2e9c841df4bf562cb6b250c2f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-08-29 18:42:58 +0200

    fix: make MuteChannel command not case sensitive

--- src/main/kotlin/commands/MuteChannel.kt
+++ src/main/kotlin/commands/MuteChannel.kt
@@ -10,24 +10,25 @@ class MuteChannel(val main: Main) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender is Player){
-        val args = args.toList().toMutableList()
-        val muteable = emptyList<String>().toMutableList()
+            val muteable = emptyList<String>().toMutableList()
 
-        args.forEach{
-            if (main.chats[it]?.muteable != null) {
-                if (main.chats[it]!!.muteable){
-                    muteable.add(main.chats[it]!!.name)
-                }else {
-                    sender.sendMessage("Channel $it is not muteable")
+
+            args.forEach{ it ->
+                val uppercaseIt = it.toUpperCase()
+                if (main.chats[uppercaseIt] != null) {
+                    if (main.chats[uppercaseIt]!!.muteable){
+                        muteable.add(main.chats[uppercaseIt]!!.name)
+                    }else {
+                        sender.sendMessage("Channel $uppercaseIt is not muteable")
+                    }
+                }else{
+                    sender.sendMessage("Channel $uppercaseIt does not exist")
                 }
-            }else{
-                sender.sendMessage("Channel $it does not exist")
             }
-        }
 
-        main.dataParser.removeStringList(muteable, "Mutet-Channels.${sender.name}")
-        main.dataParser.addStringList(muteable, "Mutet-Channels.${sender.name}")
-        return true
+            main.dataParser.removeStringList(muteable, "Mutet-Channels.${sender.name}")
+            main.dataParser.addStringList(muteable, "Mutet-Channels.${sender.name}")
+            return true
         }else{
             sender.sendMessage("You can't use that as console")
             return false
