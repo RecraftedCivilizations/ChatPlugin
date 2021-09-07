9567eb2d9ac7afb155a3f086eec9780be65044eb
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-09-07 19:37:04 +0200

    chore: Remove dependencie on multiverse

--- src/main/kotlin/com/github/darkvanityoflight/chatplugin/Main.kt
+++ src/main/kotlin/com/github/darkvanityoflight/chatplugin/Main.kt
@@ -16,11 +16,9 @@ import com.github.darkvanityoflight.chatplugin.parser.DataParser
 import net.luckperms.api.LuckPerms
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandMap
-import org.bukkit.event.Listener
 import java.io.File
 import java.lang.reflect.Field
 import com.github.darkvanityoflight.recraftedcore.ARecraftedPlugin
-import com.onarandombox.MultiverseCore.MultiverseCore
 import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.RegisteredServiceProvider
@@ -34,7 +32,6 @@ class Main : ARecraftedPlugin() {
     var factionsEnabled: Boolean = false
     var luckPermsEnabled: Boolean = false
     var luckPermApi : LuckPerms? = null
-    var multiversePlugin : MultiverseCore? = null
     var multiverseEnabled : Boolean = false
     var vaultEnabled : Boolean = false
     var economy : Economy? = null
@@ -50,12 +47,7 @@ class Main : ARecraftedPlugin() {
         // Check for apis/plugins to use
         if(Bukkit.getPluginManager().isPluginEnabled("MultiverseCore")){
             val plugin : Plugin = Bukkit.getServer().pluginManager.getPlugin("MultiverseCore")!!
-            if (plugin is MultiverseCore) {
-                multiversePlugin = plugin
-                multiverseEnabled = true
-            }else {
-                severe("Multiverse seems to be enabled on your server, but we couldn't find it")
-            }
+            multiverseEnabled = true
         }
 
         if (Bukkit.getPluginManager().isPluginEnabled("Factions")) factionsEnabled = true
